dcec86fb3192e84f1b8b4827b8d83b9fdad295ef
Siddharth Rajan <siddharthrajan10@gmail.com>
2017-09-15 00:10:34 -0400

    Improvements

--- find_sum.py
+++ find_sum.py
@@ -13,52 +13,52 @@ def get_immediate_subdirectories(a_dir):
 
 # args() parses the arguments that the user provides in the command line when running the program
 def args():
-    parser = argparse.ArgumentParser(description = "Find directory and file to sum the file\'s charges.")
-    parser.add_argument('tl', type = str, help = "Specifies the 3-letter folder in which the pH folders can be accessed.")
-    parser.add_argument('pH', type = str, help = "Specifies the pH folder where charged_output.mol2 file is.")
-    args = parser.parse_args()
-    return args
+    parser = argparse.ArgumentParser(description="Find directory and file to sum the file\'s charges.")
+    parser.add_argument('tl', type=str, help="Specifies the 3-letter folder in which the pH folders can be accessed.")
+    parser.add_argument('pH', type=str, help="Specifies the pH folder where charged_output.mol2 file is.")
+    argv = parser.parse_args()
+    return argv
 
 
 # create_data_files() generates the individual data files for each conformer
-def create_data_files(input_destination, output_destination):
-    file = open(input_destination, "r").readlines()
+def create_data_files(in_dest, out):
+    f = open(in_dest, "r").readlines()
     cutoff_list = []
-    for line_index in range(len(file)):
-        for word in file[line_index].split():
-            if(word == "@<TRIPOS>ATOM"):
+    for line_index in range(len(f)):
+        for word in f[line_index].split():
+            if word == "@<TRIPOS>ATOM":
                 cutoff_list.append(line_index)
-            elif(word == "@<TRIPOS>BOND"):
+            elif word == "@<TRIPOS>BOND":
                 cutoff_list.append(line_index)
-    for i in range(len(cutoff_list)/2):
-        output_data = open(output_destination + "/data" + str(i+1) + "d.tpl", "w")
-        data_to_write = file[cutoff_list[(i*2)]+1:cutoff_list[((i*2)+1)]]
+    for i in range(len(cutoff_list) / 2):
+        output_data = open(out + "/data" + str(i + 1) + "d.tpl", "w")
+        data_to_write = f[cutoff_list[(i * 2)] + 1:cutoff_list[((i * 2) + 1)]]
         output_data.write(''.join(data_to_write))
         output_data.close()
 
 
 # getdatafiles() returns the total number of data files for the individual conformers in the specified directory
-def getdatafiles(output_destination):
-    datafiles = [f for f in os.listdir(output_destination) if f.endswith("d.tpl")]
+def getdatafiles(out):
+    datafiles = [f for f in os.listdir(out) if f.endswith("d.tpl")]
     return len(datafiles)
 
 
 # write_charges_file() writes the charges data to the final output file
-def write_charges_file(output_destination, foldercode, pHno):
-    charges_output_file = open(output_destination + "/charges_sum.tpl", "w")
+def write_charges_file(out, foldercode, phno):
+    charges_output_file = open(out + "/charges_sum.tpl", "w")
     charges_output_file.write("##############################################################\n")
     charges_output_file.write("# Topology File for:\n")
     charges_output_file.write("# param/charges_sum.tpl\n")
-    charges_output_file.write("# " + foldercode + "_pH_" + pHno + "\n")
+    charges_output_file.write("# " + foldercode + "_pH_" + phno + "\n")
     charges_output_file.write("#\n")
     charges_output_file.write("# Created on: {}\n".format(datetime.date.today()))
     charges_output_file.write("#\n")
     charges_output_file.write("# Created with: find_sum.py by Siddharth Rajan\n")
     charges_output_file.write("#################################################################\n\n")
-    
+
     total_sum = 0
-    for i in range(1, getdatafiles(output_destination) + 1):
-        data = numpy.loadtxt(output_destination + "/data" + str(i) + "d.tpl", dtype = float, usecols = [8])
+    for i in range(1, getdatafiles(out) + 1):
+        data = numpy.loadtxt(out + "/data" + str(i) + "d.tpl", dtype=float, usecols=[8])
         charges_output_file.write("The data for conformer " + str(i) + " is..." + "\n\n")
         charges_output_file.write("[ ")
         for number in data:
@@ -71,15 +71,14 @@ def write_charges_file(output_destination, foldercode, pHno):
     charges_output_file.close()
 
 
-
 if __name__ == '__main__':
-    
+
     # Checks if this file is in the same directory as the tpl_ folder and if not, stop
     if not os.path.exists(os.getcwd() + "/tpl_"):
         sys.exit("\nError: tpl_ folder not found. Please ensure the following:\n"
                  "1: The tpl_ folder is in the same directory as find_sum.py.\n"
                  )
-    
+
     # Processes the arguments
     arguments = args()
 
@@ -96,7 +95,7 @@ if __name__ == '__main__':
 
     # Creates an empty list pH and appends the pH numbers found in all_pH to it
     pH = []
-    
+
     for p in all_pH:
         if p == "pH_4.0":
             pH.append("4.0")
@@ -110,16 +109,18 @@ if __name__ == '__main__':
     # If the provided argument for pH is not in the list of pHs, stop
     if str(arguments.pH.upper()) not in pH:
         sys.exit("\nError: No such pH for ligand. Please ensure the following:\n"
-             "1. The correct pH is specified.\n")
+                 "1. The correct pH is specified.\n")
 
     # Creates the input and output locations using the provided arguments
-    input_destination = str(os.getcwd()) + "/tpl_" + "/" + str(arguments.tl.upper()) + "/pH_" + str(arguments.pH.upper()) + "/" + str(arguments.tl.upper()) + "_pH_" + str(arguments.pH.upper()) + "-charged_output.mol2"
-    output_destination = str(os.getcwd()) + "/tpl_" + "/" + str(arguments.tl.upper()) + "/pH_" + str(arguments.pH.upper()) + "/param"
+    input_destination = str(os.getcwd()) + "/tpl_" + "/" + str(arguments.tl.upper()) + "/pH_" + str(
+        arguments.pH.upper()) + "/" + str(arguments.tl.upper()) + "_pH_" + str(
+        arguments.pH.upper()) + "-charged_output.mol2"
+    output_destination = str(os.getcwd()) + "/tpl_" + "/" + str(arguments.tl.upper()) + "/pH_" + str(
+        arguments.pH.upper()) + "/param"
 
-    # If the output place doesn't exist, stop
+    # If the output place doesn't exist, make it exist
     if not os.path.exists(output_destination):
-        sys.exit("\nError: Param folder not found. Please ensure the following:\n"
-                 "1: There is a param folder in every pH folder.\n")
+        os.makedirs(output_destination)
 
     # Creates files and specifies output location
     print("\nCreating files...\n")
@@ -128,18 +129,10 @@ if __name__ == '__main__':
     write_charges_file(output_destination, str(arguments.tl.upper()), str(arguments.pH.upper()))
 
     print("Number of conformers in {0}, pH {1}: {2}\n".format(str(arguments.tl.upper()), arguments.pH.upper(),
-                                                          str(getdatafiles(output_destination))))
+                                                              str(getdatafiles(output_destination))))
     print("The final output file is charges_sum.tpl\n")
     print("It is located in {0}\n".format(str(output_destination)))
 
     # Deletes the extra data files created in the process of making the final output file
     for a in range(1, getdatafiles(output_destination) + 1):
         os.remove(output_destination + "/data" + str(a) + "d.tpl")
-
-
-
-
-
-
-
-
